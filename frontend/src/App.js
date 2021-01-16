01f4609193e22f4041014c79864c03057dca2284
NPilis <norbertpilis@gmail.com>
2021-01-16 15:28:48 +0100

    grid layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout';
-// import { Switch, Route } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
+import Grid from './components/Grid/Grid';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const drawerWidth = 64;
 
 const useStyles = makeStyles((theme) => ({
     content: {
-        backgroundColor: '#191414',
+        backgroundColor: 'white',
         flexGrow: 1,
-        padding: theme.spacing(3),
+        padding: theme.spacing(2),
         color: '#ffffff',
         transition: theme.transitions.create('margin', {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen,
         }),
-        marginLeft: drawerWidth,
+        marginLeft: 64,
         height: '100%'
     },
     plot: {
@@ -34,12 +34,13 @@ export default function App() {
     <div className="App">
       <Layout>
       </Layout>
-      <div className={classes.content}>
-        {/* <Switch>
+      <div className={classes.content}>   
+        <Switch>
               <Route exact path="/" render={() => (
-                <Redirect to="/home" />
+                <Redirect to="/temp" />
               )} />
-        </Switch> */}
+              <Route exact path="/home" component={Grid}></Route>
+        </Switch>
       </div>
     </div>
   );
