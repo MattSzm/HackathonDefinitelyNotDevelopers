8c4c3143dbd4341e39fafd2a322061bcb580fd51
MattSzm <mateuszszmal@gmail.com>
2021-01-17 00:35:47 +0100

    basic input validation

--- backend/ai/model_wrapper.py
+++ backend/ai/model_wrapper.py
@@ -1,9 +1,28 @@
 from ai.text_rank import TextRank
+import re
+from gensim.summarization.summarizer import INPUT_MIN_LENGTH
 
-class ModelWarper:
+
+class ModelWrapper:
     def __init__(self):
         self.text_rank = TextRank()
+        self.min_length = INPUT_MIN_LENGTH
     
     def summarize(self, text:str):
-        # TODO: add model
-        return self.text_rank.summarize(text)
+        sentences = self.tokenize(text)
+        if len(sentences) > self.min_length:
+            return self.text_rank.summarize(
+                self.pre_processing_remove_newlines(
+                    self.pre_processing_remove_links(text)))
+        return ''
+
+    def pre_processing_remove_links(self, text):
+        return re.sub(r"http\S+", "", text)
+
+    def pre_processing_remove_newlines(self, text):
+        return text.replace('\n', ' ').replace('\r', '')
+
+    def tokenize(self, text):
+        sentences = re.split(r"[.!?]", text)
+        sentences = [sent.strip(" ") for sent in sentences]
+        return sentences
