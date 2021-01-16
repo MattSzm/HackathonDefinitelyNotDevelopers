c2b2a2d7b6528789d76fd437e80e8393ce70bef4
MagdalenaGie <magdalenaxgorska@gmail.com>
2021-01-16 19:41:04 +0100

    Added progres path

--- frontend/src/components/Stats/Stats.js
+++ frontend/src/components/Stats/Stats.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ProgBar from '../Converter/ProgBar';
 
 const Stats = (props) => {
     return(
         <div>
-            <ProgBar/>
+            stats
         </div>
     );
 }
